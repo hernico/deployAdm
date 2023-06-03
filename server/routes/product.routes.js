d3cee0cfa28bfa2f1aef0e846fd5eca9b88c7021
hernico <hernico@gmail.com>
2023-06-03 02:30:01 -0400

    initial commit

---
+++ server/routes/product.routes.js
@@ -0,0 +1,12 @@
+const ProductsController = require("../controllers/product.controller");
+
+module.exports = app => {
+   
+    app.post("/api/producto/nuevo", ProductsController.createProducto);
+    app.get("/api/productolist/", ProductsController.encontrarTodos);
+    app.get("/api/productolist/:id", ProductsController.encontrarUnProducto);
+    app.put("/api/productolist/actualizar/:id", ProductsController.actualizarUnProducto);
+    app.delete("/api/productolist/eliminar/:id", ProductsController.eliminarUnProducto);
+
+    
+    }
